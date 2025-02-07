ad7c9c1d7b30ddf434d83acdd724e8777229cd47
Christopher <edesonchristopher@gmail.com>
2025-02-07 11:34:39 +0100

    correct image path to public

--- components-library/src/components/testimonial-component/data.mjs
+++ components-library/src/components/testimonial-component/data.mjs
@@ -3,7 +3,7 @@ const data = [
         name: "May Andersons",
         company: "Workcation, CFO",
         img: "./public/andersons-crop.png",
-        image: "./public/andersons.jpeg",
+        image: "/public/andersons.jpeg",
         alt: "woman in glasses wearing a blue dress and smiling",
         writing: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. "
     },
@@ -11,7 +11,7 @@ const data = [
         name: "Christopher Edeson",
         company: "Workcation, CEO",
         img: "./public/chris.jpg",
-        image: "./public/attire.jpg",
+        image: "/public/attire.jpg",
         alt: "man smiling",
         writing: "As the CEO of Workation, I can confidently say we provide the best service in creating seamless workation experiences. From luxurious accommodations to tailored itineraries, we handle every detail to ensure our clients enjoy both productivity and relaxation. Our commitment to excellence and customer satisfaction sets us apart in delivering unforgettable workation experiences.",
     },
