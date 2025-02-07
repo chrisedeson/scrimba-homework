4634d716a890eb16a04402b7538771074745f9bf
Christopher <edesonchristopher@gmail.com>
2025-02-07 12:06:26 +0100

    resolve path to image

--- components-library/src/components/testimonial-component/data.mjs
+++ components-library/src/components/testimonial-component/data.mjs
@@ -2,20 +2,17 @@ const data = [
     {
         name: "May Andersons",
         company: "Workcation, CFO",
-        img: "andersons-crop.png",
-        image: "andersons.jpeg",
+        img: `${process.env.PUBLIC_URL}/andersons-crop.png`,  // Dynamically resolves path
+        image: `${process.env.PUBLIC_URL}/andersons.jpeg`,    // Dynamically resolves path
         alt: "woman in glasses wearing a blue dress and smiling",
         writing: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. "
     },
     {
         name: "Christopher Edeson",
         company: "Workcation, CEO",
-        img: "chris.jpg",
-        image: "attire.jpg",
+        img: `${process.env.PUBLIC_URL}/chris.jpg`,           // Dynamically resolves path
+        image: `${process.env.PUBLIC_URL}/attire.jpg`,        // Dynamically resolves path
         alt: "man smiling",
         writing: "As the CEO of Workation, I can confidently say we provide the best service in creating seamless workation experiences. From luxurious accommodations to tailored itineraries, we handle every detail to ensure our clients enjoy both productivity and relaxation. Our commitment to excellence and customer satisfaction sets us apart in delivering unforgettable workation experiences.",
     },
-   
 ]
-
-export default data
