942d5d6f58755574a8c5d7fc39f3bdfc50294d9c
Christopher <edesonchristopher@gmail.com>
2025-02-07 12:18:53 +0100

    resolve file path to absolute

--- components-library/src/components/testimonial-component/data.mjs
+++ components-library/src/components/testimonial-component/data.mjs
@@ -2,17 +2,22 @@ const data = [
     {
         name: "May Andersons",
         company: "Workcation, CFO",
-        img: `${process.env.PUBLIC_URL}/andersons-crop.png`,  // Dynamically resolves path
-        image: `${process.env.PUBLIC_URL}/andersons.jpeg`,    // Dynamically resolves path
+        img: "https://chris-components-library.netlify.app/andersons-crop.png",
+        image: "https://chris-components-library.netlify.app/andersons.jpeg",
         alt: "woman in glasses wearing a blue dress and smiling",
         writing: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. Amet feugiat est integer dolor auctor adipiscing nunc urna, sit. "
     },
     {
         name: "Christopher Edeson",
         company: "Workcation, CEO",
-        img: `${process.env.PUBLIC_URL}/chris.jpg`,           // Dynamically resolves path
-        image: `${process.env.PUBLIC_URL}/attire.jpg`,        // Dynamically resolves path
+        img: "https://chris-components-library.netlify.app/chris.jpg",
+        image: "https://chris-components-library.netlify.app/attire.jpg",
         alt: "man smiling",
         writing: "As the CEO of Workation, I can confidently say we provide the best service in creating seamless workation experiences. From luxurious accommodations to tailored itineraries, we handle every detail to ensure our clients enjoy both productivity and relaxation. Our commitment to excellence and customer satisfaction sets us apart in delivering unforgettable workation experiences.",
     },
+   
 ]
+
+export default data
+
+https://chris-components-library.netlify.app/andersons-crop.png
